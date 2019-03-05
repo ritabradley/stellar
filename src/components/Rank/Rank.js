db9e32d5aaf31cb43f962457ab72df7e7e0e5d4a
Rita Bradley <contact@ritabradley.com>
2019-03-04 23:19:44 -0800

    import particles.js

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const Rank = () => {
-  return <div />;
+  return (
+    <div>
+      <div className='text-light'>
+        <h4>{`Rita, your current rank is...`}</h4>
+      </div>
+      <div className='text-light'>
+        <h2>{`#2`}</h2>
+      </div>
+    </div>
+  );
 };
 
 export default Rank;
--- src/containers/App.js
+++ src/containers/App.js
@@ -4,11 +4,61 @@ import Nav from '../components/Nav/Nav';
 import Logo from '../components/Logo/Logo';
 import ImageForm from '../components/ImageForm/ImageForm';
 import Rank from '../components/Rank/Rank';
+import Particles from 'react-particles-js';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
+        <Particles
+          className='particles'
+          params={{
+            particles: {
+              number: {
+                value: 150,
+                density: {
+                  enable: true,
+                  value_area: 1000,
+                },
+              },
+              line_linked: {
+                enable: true,
+                opacity: 0.07,
+              },
+              move: {
+                direction: 'bottom-right',
+                speed: 0.3,
+              },
+              shape: {
+                type: 'star',
+              },
+              size: {
+                value: 2,
+              },
+              opacity: {
+                anim: {
+                  enable: true,
+                  speed: 1,
+                  opacity_min: 0.05,
+                },
+              },
+            },
+            interactivity: {
+              events: {
+                onclick: {
+                  enable: true,
+                  mode: 'push',
+                },
+              },
+              modes: {
+                push: {
+                  particles_nb: 1,
+                },
+              },
+            },
+            retina_detect: true,
+          }}
+        />
         <Nav />
         <Logo />
         <h1 className='title'>Stellar</h1>
