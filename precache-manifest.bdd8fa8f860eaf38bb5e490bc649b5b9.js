f9d68305071fbf9879449c38c1fbdf016b8e09b1
Rita Bradley <contact@ritabradley.com>
2019-03-20 17:48:12 -0700

    Updates

---
+++ precache-manifest.bdd8fa8f860eaf38bb5e490bc649b5b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "98d7b8e3c2d1720d3a1b8e1a2949dfa0",
+    "url": "/stellar/static/media/star.98d7b8e3.svg"
+  },
+  {
+    "revision": "e864223ad3170f0e464f",
+    "url": "/stellar/static/js/runtime~main.e864223a.js"
+  },
+  {
+    "revision": "539aa860c215a54a9255",
+    "url": "/stellar/static/js/main.539aa860.chunk.js"
+  },
+  {
+    "revision": "3ea0b8c7a2403c789b33",
+    "url": "/stellar/static/js/2.3ea0b8c7.chunk.js"
+  },
+  {
+    "revision": "539aa860c215a54a9255",
+    "url": "/stellar/static/css/main.846d23cb.chunk.css"
+  },
+  {
+    "revision": "3ea0b8c7a2403c789b33",
+    "url": "/stellar/static/css/2.4221f849.chunk.css"
+  },
+  {
+    "revision": "6552f708c60c3c35560a89673485c08e",
+    "url": "/stellar/index.html"
+  }
+];
